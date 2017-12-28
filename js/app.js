0be040afdf020e1fd5c5c2aea895c7f6ab355c37
andersp89 <anders@ptrading.dk>
2017-12-28 13:17:31 +0100

    Added more cats

--- js/app.js
+++ js/app.js
@@ -1,26 +1,58 @@
-var Cat = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.flickr.com');
-	this.levels = ko.observable('Beginner');
-	this.nickNames = ko.observableArray([
-		{ name: 'Catty cat' },
-		{ name: 'Plabby poo' },
-		{ name: 'Anderschen' }
-	]);
+// Data, eventually from DB
+var initialCats = [
+	{
+		clickCount: 0,
+		name: 'Tabby1',
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		levels: 'Beginner',
+		nickNames: [ 'Catty cat', 'Plabby poo', 'Anderschen' ]
+	},
+	{
+		clickCount: 0,
+		name: 'Tabby2',
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		levels: 'Beginner',
+		nickNames: [ 'Catty cat', 'Plabby poo', 'Anderschen' ]
+	},
+	{
+		clickCount: 0,
+		name: 'Tabby3',
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		levels: 'Beginner',
+		nickNames: [ 'Catty cat', 'Plabby poo', 'Anderschen' ]
+	}
+]
+
+// Model
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.levels = ko.observable(data.levels);
+	this.nickNames = ko.observableArray(data.nickNames);
 }
 
+// Control flow
 var ViewModel = function() {
 	var self = this;
-	this.currentCat = ko.observable( new Cat() );
+	
+	// Initiate catList and Current Cat
+	this.catList = ko.observableArray([]);
+	initialCats.forEach(function(catItem) {
+		self.catList.push( new Cat(catItem) );
+	});
+	this.currentCat = ko.observable( this.catList()[0] );
+
+	this.setCurrentCat = function(newCat) {
+		self.currentCat(newCat)
+	}
 
 	this.incrementCounter = function() {
 		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	}
 
-	self.updateLevel = function(x) {
-		self.currentCat().levels(x);
+	self.updateLevel = function(newLevel) {
+		self.currentCat().levels(newLevel);
 	}
 
 	this.findLevel = function() {
@@ -34,7 +66,5 @@ var ViewModel = function() {
 	}
 
 }
-
-
 
 ko.applyBindings(new ViewModel())
