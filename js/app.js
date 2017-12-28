b265a61c59655b682ee290dc0197541813aa5243
andersp89 <anders@ptrading.dk>
2017-12-28 11:02:26 +0100

    Seperation of concerns MVVM

--- js/app.js
+++ js/app.js
@@ -1,31 +1,38 @@
-var ViewModel = function() {
+var Cat = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('https://www.flickr.com');
 	this.levels = ko.observable();
-
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	}
-
-	this.findLevel = function() {
-		var clicks = this.clickCount();
-		if (clicks < 6) {		
-			this.levels('Beginner');
-		} else if (clicks < 11) {		
-			this.levels('Intermediate');
-		} else if (clicks < 16) {
-			this.levels('Expert')
-		}
-	}
-
-	this.nickNames = [
+	this.nickNames = ko.observableArray([
 		{ name: 'Catty cat' },
 		{ name: 'Plabby poo' },
 		{ name: 'Anderschen' }
-	];
+	]);
 }
+
+var ViewModel = function() {
+
+	this.currentCat = ko.observable( new Cat() );
+
+	this.incrementCounter = function() {
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	}
+
+	this.findLevel = function() {
+		var clicks = this.currentCat().clickCount();
+		
+		if (clicks < 6) {		
+			this.currentCat().levels('Beginner');
+		} else if (clicks < 11) {		
+			this.currentCat().levels('Intermediate');
+		} else if (clicks < 16) {
+			this.currentCat().levels('Expert')
+		}
+	}
+
+}
+
 
 
 ko.applyBindings(new ViewModel())
